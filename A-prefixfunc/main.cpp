dafc8bc91f45b479f154b98e336daa5a85fa2fc1
mitrokosta <mitrofanov.ka@phystech.edu>
2019-09-22 14:29:38 +0300

    fixed all the issues

--- A-prefixfunc/main.cpp
+++ A-prefixfunc/main.cpp
@@ -7,89 +7,80 @@ using std::endl;
 using std::string;
 using std::vector;
 
-typedef unsigned int uint;
+class Searcher {
+  public:
+    Searcher(const string& query);
+    vector<unsigned> Search(const string& text);
 
-struct Searcher {
-  vector<uint> pi;  // prefix function of the query
-  uint step;
-  uint prev;
+  private:
+    void PrefixFunc();
+    unsigned NextPref(const string& text);
+
+    string ext_query;
+    vector<unsigned> pi;  // prefix function of the query
+    unsigned step;
+    unsigned prev;
 };
-
-vector<uint> PrefixFunc(const string& source);
-Searcher* StartPrefQuery(const string& query);
-uint NextPref(Searcher* srchandler, const string& text,
-              const string& query);  // returns next prefix value
-void EndPrefQuery(Searcher* srchandler);
-vector<uint> Search(const string& text, const string& query);
 
 int main() {
   string query, text;
   cin >> query >> text;
-  for (size_t entry : Search(text, query)) {
+  Searcher srchandler = Searcher(query);
+  for (size_t entry : srchandler.Search(text)) {
     cout << entry << " ";
   }
   cout << endl;
   return 0;
 }
 
-vector<uint> PrefixFunc(const string& source) {
-  vector<uint> pi;
-  pi.reserve(source.size());
+Searcher::Searcher(const string& query) :
+    ext_query(query + "$"),
+    pi(),
+    step(0),
+    prev(0) {
+  PrefixFunc();
+}
+
+void Searcher::PrefixFunc() {
+  pi.reserve(ext_query.size());
   pi.push_back(0);  // a prefix has to be shorter than the word
-  for (int i = 1; i < static_cast<int>(source.size()); ++i) {
-    uint curr = pi[i - 1];
-    while (curr && source[i] != source[curr]) {
+  for (int i = 1; i < static_cast<int>(ext_query.size()); ++i) {
+    unsigned curr = pi[i - 1];
+    while (curr && ext_query[i] != ext_query[curr]) {
       curr = pi[curr - 1];  // looking for equal letters
     }
-    if (source[i] == source[curr]) {
+    if (ext_query[i] == ext_query[curr]) {
       ++curr;
     }
     pi.push_back(curr);
   }
-  return pi;
 }
 
-Searcher* StartPrefQuery(const string& query) {
-  Searcher* srchandler = new Searcher;
-  srchandler->pi = PrefixFunc(query);
-  srchandler->step = 0;
-  srchandler->prev = 0;
-  return srchandler;
-}
-
-uint NextPref(Searcher* srchandler, const string& text, const string& query) {
-  if (srchandler->step == text.size()) {  // just in case
+unsigned Searcher::NextPref(const string& text) {
+  if (step == text.size()) {  // just in case
     throw;
   }
-  uint curr = srchandler->prev;
-  while (curr && text[srchandler->step] !=
-                     query[curr]) {  // going back until chars match
-    curr = srchandler->pi[curr - 1];
+  unsigned curr = prev;
+  while (curr && text[step] != ext_query[curr]) {  // going back until chars match
+    curr = pi[curr - 1];
   }
-  if (text[srchandler->step] == query[curr]) {
+  if (text[step] == ext_query[curr]) {
     ++curr;
   }
-  ++(srchandler->step);
-  srchandler->prev = curr;
+  ++(step);
+  prev = curr;
   return curr;
 }
 
-void EndPrefQuery(Searcher* srchandler) {
-  delete srchandler;
-}
-
-vector<uint> Search(const string& text, const string& query) {
-  vector<uint> entries;
-  string ext_query = query;
-  ext_query += "$";
-  Searcher* srchandler = StartPrefQuery(ext_query);
-  uint pref;
+vector<unsigned> Searcher::Search(const string& text) {
+  vector<unsigned> entries;
+  unsigned pref;
   for (int i = 0; i < static_cast<int>(text.size()); ++i) {
-    pref = NextPref(srchandler, text, ext_query);
-    if (pref == query.size()) {  // entry found!
-      entries.push_back(i - query.size() + 1);
+    pref = NextPref(text);
+    if (pref == ext_query.size() - 1) {  // entry found!
+      entries.push_back(i - ext_query.size() + 2);
     }
   }
-  EndPrefQuery(srchandler);  // perform a cleanup
+  step = prev = 0; // cleanup
   return entries;
 }
