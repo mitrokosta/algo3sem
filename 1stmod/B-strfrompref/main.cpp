4eee3f82598dc145a29eb4372f07d497ebaaf7ff
mitrokosta <mitrofanov.ka@phystech.edu>
2019-09-22 14:41:32 +0300

    uploaded the correct file

--- 1stmod/B-strfrompref/main.cpp
+++ 1stmod/B-strfrompref/main.cpp
@@ -7,89 +7,41 @@ using std::endl;
 using std::string;
 using std::vector;
 
-typedef unsigned int uint;
-
-struct Searcher {
-  vector<uint> pi;  // prefix function of the query
-  uint step;
-  uint prev;
-};
-
-vector<uint> PrefixFunc(const string& source);
-Searcher* StartPrefQuery(const string& query);
-uint NextPref(Searcher* srchandler, const string& text,
-              const string& query);  // returns next prefix value
-void EndPrefQuery(Searcher* srchandler);
-vector<uint> Search(const string& text, const string& query);
+string PrefixFuncToString(const vector<unsigned>& pi);
 
 int main() {
-  string query, text;
-  cin >> query >> text;
-  for (size_t entry : Search(text, query)) {
-    cout << entry << " ";
+  vector<unsigned> pi;
+  unsigned input;
+  while (cin >> input) {
+    pi.push_back(input);
   }
-  cout << endl;
+  cout << PrefixFuncToString(pi) << endl;
   return 0;
 }
 
-vector<uint> PrefixFunc(const string& source) {
-  vector<uint> pi;
-  pi.reserve(source.size());
-  pi.push_back(0);  // a prefix has to be shorter than the word
-  for (int i = 1; i < static_cast<int>(source.size()); ++i) {
-    uint curr = pi[i - 1];
-    while (curr && source[i] != source[curr]) {
-      curr = pi[curr - 1];  // looking for equal letters
-    }
-    if (source[i] == source[curr]) {
-      ++curr;
-    }
-    pi.push_back(curr);
-  }
-  return pi;
-}
-
-Searcher* StartPrefQuery(const string& query) {
-  Searcher* srchandler = new Searcher;
-  srchandler->pi = PrefixFunc(query);
-  srchandler->step = 0;
-  srchandler->prev = 0;
-  return srchandler;
-}
-
-uint NextPref(Searcher* srchandler, const string& text, const string& query) {
-  if (srchandler->step == text.size()) {  // just in case
-    throw;
-  }
-  uint curr = srchandler->prev;
-  while (curr && text[srchandler->step] !=
-                     query[curr]) {  // going back until chars match
-    curr = srchandler->pi[curr - 1];
-  }
-  if (text[srchandler->step] == query[curr]) {
-    ++curr;
-  }
-  ++(srchandler->step);
-  srchandler->prev = curr;
-  return curr;
-}
-
-void EndPrefQuery(Searcher* srchandler) {
-  delete srchandler;
-}
-
-vector<uint> Search(const string& text, const string& query) {
-  vector<uint> entries;
-  string ext_query = query;
-  ext_query += "$";
-  Searcher* srchandler = StartPrefQuery(ext_query);
-  uint pref;
-  for (int i = 0; i < static_cast<int>(text.size()); ++i) {
-    pref = NextPref(srchandler, text, ext_query);
-    if (pref == query.size()) {  // entry found!
-      entries.push_back(i - query.size() + 1);
+string PrefixFuncToString(const vector<unsigned>& pi) {
+  char chr = 'a'; // current char for zeroes
+  string result;
+  result.reserve(pi.size());
+  result.push_back(chr);
+  for (int i = 1; i < static_cast<int>(pi.size()); ++i) {
+    if (pi[i] == 0) {
+      chr = 'a';
+      while (true) { // looking for the first char that won't spoil the prefix func
+        int j = i;
+        while (j && result[pi[j - 1]] != chr) {
+          j = pi[j - 1];
+        }
+        if (j == 0) {
+          break;
+        } else {
+          ++chr;
+        }
+      }
+      result.push_back(chr);
+    } else {
+      result.push_back(result[pi[i] - 1]); // simply append the corresponding char
     }
   }
-  EndPrefQuery(srchandler);  // perform a cleanup
-  return entries;
+  return result;
 }
