852cb294e7a37873fca1910eeef7b73cd92cb3d0
mitrokosta <mitrofanov.ka@phystech.edu>
2019-09-29 00:29:01 +0300

    added a separate function (FirstGoodLetter)

--- 1stmod/B-strfrompref/main.cpp
+++ 1stmod/B-strfrompref/main.cpp
@@ -8,6 +8,7 @@ using std::string;
 using std::vector;
 
 string PrefixFuncToString(const vector<unsigned>& pi);
+char FirstGoodLetter(const string& result, int start_pos);
 
 int main() {
   vector<unsigned> pi;
@@ -19,25 +20,30 @@ int main() {
   return 0;
 }
 
+char FirstGoodLetter(const vector<unsigned>& pi, const string& result, int start_pos) {
+  char chr = 'a';
+  while (true) { // looking for the first char that won't spoil the prefix func
+    int j = start_pos;
+    while (j && result[pi[j - 1]] != chr) {
+      j = pi[j - 1];
+    }
+    if (j == 0) {
+      break;
+    } else {
+      ++chr;
+    }
+  }
+  return chr;
+}
+
 string PrefixFuncToString(const vector<unsigned>& pi) {
-  char chr = 'a'; // current char for zeroes
+  char chr = 'a';
   string result;
   result.reserve(pi.size());
   result.push_back(chr);
   for (int i = 1; i < static_cast<int>(pi.size()); ++i) {
     if (pi[i] == 0) {
-      chr = 'a';
-      while (true) { // looking for the first char that won't spoil the prefix func
-        int j = i;
-        while (j && result[pi[j - 1]] != chr) {
-          j = pi[j - 1];
-        }
-        if (j == 0) {
-          break;
-        } else {
-          ++chr;
-        }
-      }
+      chr = FirstGoodLetter(pi, result, i);
       result.push_back(chr);
     } else {
       result.push_back(result[pi[i] - 1]); // simply append the corresponding char
